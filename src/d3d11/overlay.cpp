436afd9d6e1a032636573f9879b7962c712da5f5
rohit <rohitsoni259@gmail.com>
2026-05-26 20:53:15 +0530

    d3d11 overlay

--- src/d3d11/overlay.cpp
+++ src/d3d11/overlay.cpp
@@ -282,6 +282,8 @@ void OnDXPresent(IDXGISwapChain* pSwapChain) {
         bool shouldLog = (frameCount11 < 5);
         if (shouldLog)
             Logger::info("DX11 Frame " + std::to_string(frameCount11));
+        
+        ImGuiIO& io = ImGui::GetIO();
 
         ImGui_ImplDX11_NewFrame();
         ImGui_ImplWin32_NewFrame();
